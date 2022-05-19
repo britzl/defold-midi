9e94350cb8ceb862dd367a95eb307e2b804097f7
Brian <subsoap@gmail.com>
2022-05-19 03:27:19 -0700

    disable other platforms

--- midi/src/DefoldMIDI.cpp
+++ midi/src/DefoldMIDI.cpp
@@ -1,12 +1,13 @@
+#define EXTENSION_NAME DefoldMIDI
 #define LIB_NAME "DefoldMIDI"
-#define MODULE_NAME "midi"
+#define MODULE_NAME "DefoldMIDI"
 
 #ifndef DLIB_LOG_DOMAIN
-#define DLIB_LOG_DOMAIN "midi"
+#define DLIB_LOG_DOMAIN LIB_NAME
 #endif
 #include <dmsdk/sdk.h>
 
-#if defined(DM_PLATFORM_OSX) || defined(DM_PLATFORM_WINDOWS) || defined(DM_PLATFORM_LINUX) || defined(DM_PLATFORM_IOS) || defined(DM_PLATFORM_HTML5)
+#if defined(DM_PLATFORM_WINDOWS) || defined(DM_PLATFORM_HTML5)
 
 #include "RtMidi.h"
 
@@ -29,17 +30,11 @@ dmExtension::Result FinalizeDefoldMIDI(dmExtension::Params* params) {
 void OnEventDefoldMIDI(dmExtension::Params* params, const dmExtension::Event* event) {
 }
 
+DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, AppInitializeDefoldMIDI, 0, InitializeDefoldMIDI, UpdateDefoldMIDI, OnEventDefoldMIDI, FinalizeDefoldMIDI)
+
 #else
 
-dmExtension::Result AppInitializeDefoldMIDI(dmExtension::AppParams* params) {
-	return dmExtension::RESULT_OK;
-}
-
 dmExtension::Result InitializeDefoldMIDI(dmExtension::Params* params) {	
-	return dmExtension::RESULT_OK;
-}
-
-dmExtension::Result UpdateDefoldMIDI(dmExtension::Params* params) {
 	return dmExtension::RESULT_OK;
 }
 
@@ -47,9 +42,6 @@ dmExtension::Result FinalizeDefoldMIDI(dmExtension::Params* params) {
 	return dmExtension::RESULT_OK;
 }
 
-void OnEventDefoldMIDI(dmExtension::Params* params, const dmExtension::Event* event) {
-}
+DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, 0, 0, InitializeDefoldMIDI, 0, 0, FinalizeDefoldMIDI)
 
 #endif
-
-DM_DECLARE_EXTENSION(DefoldMIDI, LIB_NAME, AppInitializeDefoldMIDI, 0, InitializeDefoldMIDI, UpdateDefoldMIDI, OnEventDefoldMIDI, FinalizeDefoldMIDI)
