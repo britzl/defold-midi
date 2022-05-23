a283a60227ec62a5d917db67369f87c5bb05398c
Björn Ritzl <bjorn.ritzl@gmail.com>
2022-05-24 00:35:57 +0200

    Started adding the functions

--- midi/src/DefoldMIDI.cpp
+++ midi/src/DefoldMIDI.cpp
@@ -7,21 +7,103 @@
 #endif
 #include <dmsdk/sdk.h>
 
-#if defined(DM_PLATFORM_WINDOWS) || defined(DM_PLATFORM_HTML5)
+#if defined(DM_PLATFORM_WINDOWS) || defined(DM_PLATFORM_HTML5) || defined(DM_PLATFORM_OSX)
 
 #include "RtMidi.h"
+#include <cstdlib>
 
-static int Test(lua_State* L)
+static RtMidiIn  *g_midiin = 0;
+static RtMidiOut *g_midiout = 0;
+
+static int CountOut(lua_State* L)
 {
-	//
-	printf("test\n");
+	DM_LUA_STACK_CHECK(L, 1);
+	int count = g_midiout->getPortCount();
+	lua_pushnumber(L, count);
+	return 1;
+}
+
+static int CountIn(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 1);
+	int count = g_midiin->getPortCount();
+	lua_pushnumber(L, count);
+	return 1;
+}
+
+static int OpenOut(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	int port = luaL_checknumber(L, 1);
+	g_midiout->openPort(port);
+	return 0;
+}
+
+static int OpenIn(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	int port = luaL_checknumber(L, 1);
+	g_midiin->openPort(port);
+	return 0;
+}
+
+static int OpenVirtualOut(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	const char* name = luaL_checkstring(L, 1);
+	g_midiout->openVirtualPort(name);
+	return 0;
+}
+
+static int OpenVirtualIn(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	const char* name = luaL_checkstring(L, 1);
+	g_midiin->openVirtualPort(name);
+	return 0;
+}
+
+static int CloseOut(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	g_midiout->closePort();
+	return 0;
+}
+
+static int CloseIn(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 0);
+	g_midiin->closePort();
+	return 0;
+}
+
+static int GetMessages(lua_State* L)
+{
+	DM_LUA_STACK_CHECK(L, 1);
+	std::vector<unsigned char> message;
+	double stamp = g_midiin->getMessage( &message );
+	int nBytes = message.size();
+	lua_newtable(L);
+	for ( int i=0; i<nBytes; i++ )
+	{
+		lua_pushnumber(L, (int)message[i]);
+		lua_rawseti(L, -2, 1);
+	}
 	return 1;
 }
 
 // Functions exposed to Lua
 static const luaL_reg Module_methods[] =
 {
-	{"test", Test},
+	{"count_out", CountOut},
+	{"count_in", CountIn},
+	{"open_out", OpenOut},
+	{"open_in", OpenIn},
+	{"open_virtual_out", OpenVirtualOut},
+	{"open_virtual_in", OpenVirtualIn},
+	{"close_out", CloseOut},
+	{"close_in", CloseIn},
+	{"get_messages", GetMessages},
 	{0, 0}
 };
 
@@ -41,8 +123,11 @@ dmExtension::Result AppInitializeDefoldMIDI(dmExtension::AppParams* params) {
 }
 
 dmExtension::Result InitializeDefoldMIDI(dmExtension::Params* params) {
+	g_midiin = new RtMidiIn();
+	g_midiout = new RtMidiOut();
+	
 	LuaInit(params->m_L);
-	dmLogInfo("Registered Defold MIDI Extension", MODULE_NAME);
+	dmLogInfo("Registered Defold MIDI Extension %s", MODULE_NAME);
 	return dmExtension::RESULT_OK;
 }
 
@@ -51,6 +136,16 @@ dmExtension::Result UpdateDefoldMIDI(dmExtension::Params* params) {
 }
 
 dmExtension::Result FinalizeDefoldMIDI(dmExtension::Params* params) {
+	if (g_midiin)
+	{
+		delete g_midiin;
+		g_midiin = 0;
+	}
+	if (g_midiout)
+	{
+		delete g_midiout;
+		g_midiout = 0;
+	}
 	return dmExtension::RESULT_OK;
 }
 
