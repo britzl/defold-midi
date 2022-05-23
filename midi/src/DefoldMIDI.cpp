adcf8d982d15eefff0f6865f48b7c07b0a3b9d48
Brian <subsoap@gmail.com>
2022-05-23 01:50:46 -0700

    misc stuff

--- midi/src/DefoldMIDI.cpp
+++ midi/src/DefoldMIDI.cpp
@@ -1,6 +1,6 @@
-#define EXTENSION_NAME DefoldMIDI
-#define LIB_NAME "DefoldMIDI"
-#define MODULE_NAME "DefoldMIDI"
+#define EXTENSION_NAME midi
+#define LIB_NAME "midi"
+#define MODULE_NAME "midi"
 
 #ifndef DLIB_LOG_DOMAIN
 #define DLIB_LOG_DOMAIN LIB_NAME
@@ -11,11 +11,38 @@
 
 #include "RtMidi.h"
 
+static int Test(lua_State* L)
+{
+	//
+	printf("test\n");
+	return 1;
+}
+
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{
+	{"test", Test},
+	{0, 0}
+};
+
+static void LuaInit(lua_State* L)
+{
+	int top = lua_gettop(L);
+
+	// Register lua names
+	luaL_register(L, MODULE_NAME, Module_methods);
+
+	lua_pop(L, 1);
+	assert(top == lua_gettop(L));
+}
+
 dmExtension::Result AppInitializeDefoldMIDI(dmExtension::AppParams* params) {
 	return dmExtension::RESULT_OK;
 }
 
-dmExtension::Result InitializeDefoldMIDI(dmExtension::Params* params) {	
+dmExtension::Result InitializeDefoldMIDI(dmExtension::Params* params) {
+	LuaInit(params->m_L);
+	dmLogInfo("Registered Defold MIDI Extension", MODULE_NAME);
 	return dmExtension::RESULT_OK;
 }
 
