961d41d2b4af8acbcdb50741adf6ce2607ba0f98
Brian <subsoap@gmail.com>
2022-05-25 18:07:31 -0700

    add solfège syllables

--- midi/helpers/midi_tools.lua
+++ midi/helpers/midi_tools.lua
@@ -2,6 +2,8 @@ local M = {}
 
 M.NOTE_NAMES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
 
+M.NOTE_NAMES_SOLFEGE = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa", "Sol", "Sol#", "La", "La#", "Si"}
+
 M.octave_modifier = 1
 -- 1 matches 60 / middle C to C4
 -- 2 matches 60 / middle C to C3 and so on
@@ -10,6 +12,14 @@ function M.number_to_name(note_number, include_octave)
 	local name  = M.NOTE_NAMES[note_number % 12 + 1]
 	if include_octave then 
 		name = name .. math.floor(note_number / 12 - M.octave_modifier)
+	end
+	return name
+end
+
+function M.number_to_name_solfege(note_number, include_octave)
+	local name  = M.NOTE_NAMES_SOLFEGE[note_number % 12 + 1]
+	if include_octave then 
+		name = name .. " " .. math.floor(note_number / 12 - M.octave_modifier)
 	end
 	return name
 end
